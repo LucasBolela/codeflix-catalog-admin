e3bd1d733456dc49506804b14a6c6aa1874d51b9
Lucas Bolela <lucasbolelasilveira@gmail.com>
2024-03-29 15:30:51 -0300

    test[DeleteCategory]: integration, unit and infra

--- src/core/category/tests/infra/test_in_memory_category_repository.py
+++ src/core/category/tests/infra/test_in_memory_category_repository.py
@@ -29,3 +29,17 @@ class TestGetById:
             description=category.description,
             is_active=category.is_active,
         )
+
+
+class TestDeleteCategory:
+    def test_can_delete_category_by_id(self):
+        category = Category(name="Filme", description="Categoria para filmes")
+        repository = InMemoryCategoryRepository(categories=[category])
+
+        assert len(repository.categories) == 1
+        assert repository.get_by_id(id=category.id) is not None
+        response = repository.delete(id=category.id)
+
+        assert len(repository.categories) == 0
+        assert repository.get_by_id(id=category.id) is None
+        assert response is None
