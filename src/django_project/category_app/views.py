ddd052911c5bc30c6738e60f2ae3f898beccc18a
Lucas Bolela <lucasbolelasilveira@gmail.com>
2024-03-30 19:08:03 -0300

    feat[category-view]: implement retreive of category

--- src/django_project/category_app/views.py
+++ src/django_project/category_app/views.py
@@ -1,8 +1,14 @@
+from uuid import UUID
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.request import Request
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND
 
+from src.core.category.application.use_cases.exceptions import CategoryNotFound
+from src.core.category.application.use_cases.get_category import (
+    GetCategory,
+    GetCategoryRequest,
+)
 from src.core.category.application.use_cases.list_category import (
     ListCategory,
     ListCategoryRequest,
@@ -31,3 +37,24 @@ class CategoryViewSet(viewsets.ViewSet):
             status=HTTP_200_OK,
             data=categories,
         )
+
+    def retrieve(self, request: Request, pk=None) -> Response:
+        try:
+            category_pk = UUID(pk)
+        except ValueError:
+            return Response(status=HTTP_400_BAD_REQUEST)
+
+        use_case = GetCategory(repository=DjangoORMCategoryRepository())
+
+        try:
+            result = use_case.execute(request=GetCategoryRequest(id=category_pk))
+        except CategoryNotFound:
+            return Response(status=HTTP_404_NOT_FOUND)
+
+        category_output = {
+            "id": str(result.id),
+            "name": result.name,
+            "description": result.description,
+            "is_active": result.is_active,
+        }
+        return Response(status=HTTP_200_OK, data=category_output)
