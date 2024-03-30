a115e10bbfe528164a04221b83bc057d9f2659c1
Lucas Bolela <lucasbolelasilveira@gmail.com>
2024-03-30 13:45:07 -0300

    refact[repository]: change category_repository folder structure

--- src/core/category/tests/application/unit/test_create_category.py
+++ src/core/category/tests/application/unit/test_create_category.py
@@ -3,7 +3,7 @@ from uuid import UUID
 from unittest.mock import MagicMock
 
 
-from src.core.category.application.category_repository import CategoryRepository
+from src.core.category.domain.category_repository import CategoryRepository
 from src.core.category.application.use_cases.create_category import (
     CreateCategory,
     CreateCategoryRequest,
--- src/core/category/tests/application/unit/test_get_category.py
+++ src/core/category/tests/application/unit/test_get_category.py
@@ -4,7 +4,7 @@ from uuid import UUID
 from unittest.mock import create_autospec
 
 
-from src.core.category.application.category_repository import CategoryRepository
+from src.core.category.domain.category_repository import CategoryRepository
 from src.core.category.application.use_cases.exceptions import CategoryNotFound
 from src.core.category.application.use_cases.get_category import (
     GetCategory,
