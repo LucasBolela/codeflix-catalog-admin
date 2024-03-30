6db1130c9b18d4dd633a795eb53b38c4b4887ba9
Lucas Bolela <lucasbolelasilveira@gmail.com>
2024-03-30 16:34:28 -0300

    feat[category-view]: implement list of categories and add url

--- src/django_project/urls.py
+++ src/django_project/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
+from rest_framework.routers import DefaultRouter
+
+from src.django_project.category_app.views import CategoryViewSet
+
+router = DefaultRouter()
+router.register(r"api/categories", CategoryViewSet, basename="category")
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+] + router.urls
